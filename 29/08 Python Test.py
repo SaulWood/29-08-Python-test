ed22af12cbe3d1180296b4a373dedbff6c3e1e0c
saul-wood <saulswood@gmail.com>
2025-08-28 21:23:11 +0100

    Wrote another function, a real one this tim albeit useless to shout out base letters.

--- 29/08 Python Test.py
+++ 29/08 Python Test.py
@@ -4,3 +4,18 @@ def new_function (funky, clunky):
 
 vaR = new_function(1,2)
 print(vaR)
+gene_seq = 'ACTGCTAGCTATCGATCTATATCGCTAGCTACGCGTAGCTACTAATATATCGCGCGGATCGTACTAGCAGGAGACTCTCTAGCACTACACGCGCTAGCTAGC'
+
+for i in gene_seq:
+    if i == 'A':
+        print('We have an A')
+    elif i == 'C':
+        print('We have a C')
+    elif i == 'T':
+        print('We have a T')
+    elif i == 'G':
+        print('We have a G')
+    else:
+        print('We do not have an A/C/T/G')
+
+
