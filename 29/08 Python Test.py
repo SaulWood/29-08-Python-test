d814edb11bdd58619a746b1b3f8e12dafa40484c
saul-wood <saulswood@gmail.com>
2025-08-28 21:10:40 +0100

    comet

---
+++ 29/08 Python Test.py
@@ -0,0 +1,19 @@
+PLEASE PLEASE PLEASE work with me git hub
+awfaw
+faw
+from awf
+aw
+from awf
+aw\
+    fv\
+        efwse
+         fawefg
+         \ReferenceErrorweFQAWFAW
+AWF
+AW
+FalseAWF
+AWF
+AW
+F|a
+AWF
+ AW
