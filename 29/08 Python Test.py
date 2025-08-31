d82f03a101a6748887395921211a93754cd543ab
saul-wood <saulswood@gmail.com>
2025-08-31 16:03:51 +0100

    Second code push

--- 29/08 Python Test.py
+++ 29/08 Python Test.py
@@ -19,3 +19,19 @@ for i in gene_seq:
         print('We do not have an A/C/T/G')
 
 
+vaR = new_function(1,2)
+print(vaR)
+gene_seq = 'ACTGCTAGCTATCGATCTATATCGCTAGCTACGCGTAGCTACTAATATATCGCGCGGATCGTACTAGCAGGAGACTCTCTAGCACTACACGCGCTAGCTAGC'
+
+for i in gene_seq:
+    if i == 'A':
+        print('We have an A')
+    elif i == 'C':
+        print('We have a C')
+    elif i == 'T':
+        print('We have a T')
+    elif i == 'G':
+        print('We have a G')
+    else:
+        print('We do not have an A/C/T/G')
+
